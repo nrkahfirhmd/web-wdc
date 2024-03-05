2eaf2d3dfd2247027e9a221c400fc6d3a3bad051
Nurkahfi A Rahmada <110613182+Sakyta@users.noreply.github.com>
2024-03-05 17:34:24 +0700

    Footer and Nav Responsive Done

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+var hamburger = document.querySelector('.hamburger');
+var responsive = document.querySelector('.resp')
+
+function displayMenu() 
+{
+    hamburger.classList.toggle('kebuka');
+    responsive.classList.toggle('ciluk-ba')
+}
